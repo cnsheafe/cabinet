3197e7110dc77fc10b7d47794599e40049214b0a
Christofer Sheafe <cnsheafe@gmail.com>
2018-01-28 20:01:42 -0700

    Use beforeEach to reduce boilerplate and add test for decrement

--- src/hello-world/__tests__/component.test.tsx
+++ src/hello-world/__tests__/component.test.tsx
@@ -1,43 +1,45 @@
 import * as React from 'react'
-import { shallow, configure } from 'enzyme'
+import { shallow, configure, ShallowWrapper } from 'enzyme'
 import * as Adapter from 'enzyme-adapter-react-16'
 
-import { HelloWorldComponent, mapStateToProps } from '../'
+import { HelloWorldComponent, mapStateToProps, StateProps, DispatchProps } from '../'
 import { State } from 'store'
 import { createHelloWorldRecord as createRecord } from '../reducer'
 configure({ adapter: new Adapter() })
 
 describe('HelloWorld', () => {
-  let mockInc = jest.fn()
-  let decInc = jest.fn()
+  let incFn =  jest.fn()
+  let decFn = jest.fn()
+  let props: StateProps & DispatchProps
+  let wrapper: ShallowWrapper<any, any>
+
   beforeEach(() => {
-    mockInc = jest.fn()
-    decInc = jest.fn()
+    incFn = jest.fn()
+    decFn = jest.fn()
+    props = {
+      counter: 0,
+      inc: incFn,
+      dec: decFn,
+    }
+    wrapper = shallow(<HelloWorldComponent {...props} />)
   })
 
   describe('<HelloWorldComponent />', () => {
     it('should match the snapshot', () => {
-      const wrapper = shallow(
-        <HelloWorldComponent 
-          counter={0} 
-          inc={mockInc} 
-          dec={decInc}
-        />)
       expect(wrapper).toMatchSnapshot()
     })
-    it('should call increment when button is clicked', () => {
-      const wrapper = shallow(
-        <HelloWorldComponent 
-          counter={0}
-          inc={mockInc}
-          dec={decInc}
-        />
-      )
+    it('should call increment when increment button is clicked', () => {
       wrapper.find('button').first().simulate('click', () => {
         return
       })
-      expect(mockInc).toHaveBeenCalled()
+      expect(incFn).toHaveBeenCalled()
     })
+    it('should call decrement when decrement button is clicked', () => {
+      wrapper.find('button').last().simulate('click', () => {
+        return
+      })
+      expect(decFn).toHaveBeenCalled()     
+    });
   })
 
   describe('mapStateToProps', () => {
@@ -49,8 +51,8 @@ describe('HelloWorld', () => {
     });
     
     it('should map count from state', () => {
-      const props = mapStateToProps(state, {})
-      expect(props.counter).toBe(0)
+      const mockProps = mapStateToProps(state, {})
+      expect(mockProps.counter).toBe(0)
     })
   })
 })
